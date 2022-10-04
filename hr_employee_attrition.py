9e7a896e554ecbc4d3badcb9237d7afa755ce127
Waleed Almuhlil <walmalki@yahoo.com>
2022-10-04 11:57:20 +0300

    init

---
+++ hr_employee_attrition.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+df = pd.read_excel('hr_employee_attrition.xlsx', sheet_name='HR Employee Attrition')
+profile = ProfileReport(df, title='HR Employee Attrition')
+profile.to_file('HR_Employee_Attrition_Analysis.html')
