b61a4ba2456b461bedf61bbb73e8257c8f53fe8b
Waleed <walmalki@yahoo.com>
2022-10-11 15:00:56 +0300

    upload index file both dirs

--- hotel_bookings/hotel_bookings.py
+++ hotel_bookings/hotel_bookings.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv('hotel_bookings.csv', sheet_name='hotel_bookings')
+df = pd.read_csv('hotel_bookings.csv')
 profile = ProfileReport(df, title='Hotel Bookings | Data Analysis')
 profile.to_file('index.html')
