a0f4ca07f3037f0f9fc1a210471f405ab7446978
Waleed <walmalki@yahoo.com>
2022-10-30 16:07:12 +0300

    update index.html

--- bitcoin_and_cryptocurrencies/bitcoin_cryptocurrencies.py
+++ bitcoin_and_cryptocurrencies/bitcoin_cryptocurrencies.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv('coinmarketcap_06012018.csv')
+df = pd.read_csv('coinmarketcap_06122017.csv')
 profile = ProfileReport(df, title='Bitcoin & Cryptocurrencies | Data Analysis')
 profile.to_file('index.html')
